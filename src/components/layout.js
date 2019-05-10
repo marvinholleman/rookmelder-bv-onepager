bd840a70409c0b53f6cb22464d09235f45bc3017
Marvin Holleman <marvinholleman@145-137-91-150.wlan.hro.nl>
2019-05-10 10:37:20 +0200

    Add styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import styled from 'styled-components';
 
 import Header from "./header"
 import "./layout.css"
@@ -26,14 +27,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
